fc27f9ef2f30c15b6f3d6734edd729d41fa20af7
Carlos F <103224983+F3nr1@users.noreply.github.com>
2022-06-22 22:26:27 -0500

    Update Exercise7.java
    Program which find how many times a character in  on a string

--- Exercise7.java
+++ Exercise7.java
@@ -12,26 +12,35 @@ import java.util.Scanner;
  */
 public class Exercise7 {
     
-    //1. Creamos la instancia de la librería Scanner
+    //1. Scanner Instance
     Scanner scannerInstance = new Scanner(System.in);
-    //2. Intancias de cadena y caracter a buscar
+    
+    //2. Empty vars
+    //Name  string var
     private String name;
+    // Character char var
     private char findCharacter;
+    // Counter of chars found set int = 0
     private int totalCharacters = 0;
+    // temp storage of char 
     private char temp;
      
     
 
-    
+    //2. Char finder method
     public void characterFinder(){
+        //Print  msg
         System.out.println("BUSCADOR DE CARACTERES EN NOMBRE. ");
+        // Print the user should write a name
         System.out.println("    Ingrese su nombre: ");
+        //Scanner instance for user string capture
         name = scannerInstance.nextLine();
+        //msg for character to find
         System.out.println("    Ingrese el caracter a buscar: ");
-        
+        // Scanner instance for character to look for 
         findCharacter = scannerInstance.next().charAt(0);
         
-        
+        //3. For loop to counter how many times character which have been put by the used is inside a string
         for (int i = 0; i < name.length(); i++) {
 
             temp = name.charAt(i);
@@ -43,6 +52,9 @@ public class Exercise7 {
         
         
             }
+        
+        //4. print output of the search
+        
         System.out.println("    Nombre: "+name);
         System.out.println("    Caracter buscado: "+findCharacter);
         System.out.println("    Cantidad de ocurrencias: "+totalCharacters); 
